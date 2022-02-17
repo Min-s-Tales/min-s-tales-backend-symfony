cd592e81d57a5014b8ac9faf006cf7564af51a72
Theovady Moutty <theomoutty@gmail.com>
2022-02-17 14:33:23 +0100

    first commit

---
+++ src/Controller/UsersController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Controller;
+
+
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use App\Service\UsersService;
+use JMS\Serializer\SerializerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+
+
+class UsersController extends ApiBaseController
+{
+    /**
+     * @Route(path="/users/register", name="register_users", methods={"Post"})
+     */
+    public function register(
+        UsersService $usersService,
+        UserPasswordHasherInterface $passwordHasher,
+        SerializerInterface $serializer,
+        Request $request): JsonResponse
+    {
+        $result = false;
+        $user = new User();
+        /** @var User $user */
+        $user = $serializer->deserialize($request->getContent(), User::class, 'json');
+
+        $hashedPassword = $passwordHasher->hashPassword($user ,$user->getPassword());
+        $user->setPassword($hashedPassword);
+        $result = $usersService->create($user);
+
+        return $this->json(
+            [
+                'result' => $result,
+            ],
+            $result ? Response::HTTP_CREATED : Response::HTTP_BAD_REQUEST,
+        );
+    }
+
+    /**
+     * @Route(path="/users/login", name="login_users", methods={"Post"})
+     */
+    public function login(
+        UserRepository $userRepository,
+        UsersService $usersService,
+        UserPasswordHasherInterface $passwordHasher,
+        SerializerInterface $serializer,
+        Request $request): JsonResponse
+    {
+        $result = false;
+        $user = new User();
+        $email = $request->request->get('email');
+        $password = $request->request->get('password');
+
+
+        $result = $usersService->login($user);
+
+        return $this->json(
+            [
+                'user' => $user,
+                'result' => $result,
+            ],
+            $result ? Response::HTTP_CREATED : Response::HTTP_BAD_REQUEST,
+        );
+    }
+
+
+
+}
