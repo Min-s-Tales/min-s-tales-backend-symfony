9256af509595f543b8602aab028c556eb6eff781
Theovady Moutty <theomoutty@gmail.com>
2022-02-18 17:35:58 +0100

    delete testfailure in create user test

--- tests/User/CreateUserTest.php
+++ tests/User/CreateUserTest.php
@@ -33,32 +33,4 @@ class CreateUserTest extends WebTestCase
             ]
         ];
     }
-
-    /**
-     * @dataProvider fieldCreatedFailureProvider
-     */
-    public function testCreatedTagFailure($data, $errors)
-    {
-        $response = $this->jsonRequest('POST', '/api/v1/items/tags', $data);
-        $this->assertJson($response->getContent());
-        $this->assertSame(Response::HTTP_BAD_REQUEST, $response->getStatusCode());
-        $responseContent = json_decode($response->getContent(), true);
-        $this->assertSame(false, $responseContent['result']);
-
-        foreach ($errors as $e) {
-            $this->errorContainsField($e, $responseContent['errors']);
-        }
-    }
-
-    public function fieldCreatedFailureProvider()
-    {
-        yield [
-            ['name' => ''],
-            ['name']
-        ];
-        yield [
-            ['name' => 'a'],
-            ['name']
-        ];
-    }
 }
